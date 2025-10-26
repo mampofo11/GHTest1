f12862229a476e1ca09c49e891b009e833590c1b
Marvin Ampofo <marvinampofo11@gmail.com>
2025-10-25 23:23:55 -0400

    i changed it fn

--- test1.java
+++ test1.java
@@ -3,5 +3,14 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Uzumaki Naruto, DATTEBAYO!");
         System.out.println("Baba boeey");
+
+        int n = 1;
+        if (n < 5) {
+            System.out.println(" I AM #1");
+        }
+        else {
+            System.out.println("aye mane say mane");
+        }
+
     }
 }
