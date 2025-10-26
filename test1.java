0ae053b81f3d8d810f87e12519dc950f61943447
Marvin Ampofo <marvinampofo11@gmail.com>
2025-10-25 23:16:22 -0400

    blah blah blah

--- test1.java
+++ test1.java
@@ -1,6 +1,6 @@
 // Basic Java Boilerplate
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Uzumaki Naruto, DATTEBAYO!");
     }
 }
